6857888d1b7db03e84a5a5264fdbccdc13695f4e
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-02 15:26:48 +0300

    change app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { selectThemeMode } from "@/app/app-slice"
+import { selectThemeMode, setIsLoggedInAC } from "@/app/app-slice"
 import { ErrorSnackbar, Header } from "@/common/components"
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { Routing } from "@/common/routing"
@@ -11,7 +11,6 @@ import { useEffect, useState } from "react"
 import styles from "./App.module.css"
 import { useMeQuery } from "@/features/auth/api/authApi.ts"
 import { ResultCode } from "@/common/enums"
-import { setIsLoggedInAC } from "@/features/auth/model/auth-slice.ts"
 
 export const App = () => {
 

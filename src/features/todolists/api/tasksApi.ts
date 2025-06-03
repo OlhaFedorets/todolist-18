8b837642db63a14475fc6cc6e7d6fa82797b673b
OlhaFedorets <fedoretsolha@gmail.com>
2025-06-03 11:46:55 +0300

    change tasksApi

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,8 +1,47 @@
 import { instance } from "@/common/instance"
 import type { BaseResponse } from "@/common/types"
 import type { DomainTask, GetTasksResponse, UpdateTaskModel } from "./tasksApi.types"
+import { baseApi } from "@/app/baseApi.ts"
 
-export const tasksApi = {
+
+export const tasksApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getTasks: build.query<GetTasksResponse, string>({
+      query: (todolistId) => `/todo-lists/${todolistId}/tasks`,
+      providesTags: ['Tasks']
+    }),
+    createTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
+      query: ({ todolistId, title }) => ({
+        url: `/todo-lists/${todolistId}/tasks`,
+        method: "POST",
+        body: {title}
+      }),
+      invalidatesTags: ['Tasks']
+    }),
+    updateTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; taskId: string; model: UpdateTaskModel }>({
+      query: ({ todolistId, taskId, model }) => ({
+        url: `/todo-lists/${todolistId}/tasks/${taskId}`,
+        method: "PUT",
+        body: model
+      }),
+      invalidatesTags: ['Tasks']
+    }),
+    deleteTask: build.mutation<BaseResponse, { todolistId: string; taskId: string }>({
+      query: ({todolistId, taskId}) => ({
+        url: `/todo-lists/${todolistId}/tasks/${taskId}`,
+        method: "DELETE"
+      }),
+      invalidatesTags: ['Tasks']
+    })
+  })
+})
+
+
+export const {useGetTasksQuery, useCreateTaskMutation, useUpdateTaskMutation, useDeleteTaskMutation} = tasksApi
+
+
+
+export const _tasksApi = {
   getTasks(todolistId: string) {
     return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
   },
